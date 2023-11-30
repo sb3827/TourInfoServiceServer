6a55b180104b2e3f6b405d4e07ecc28b6faf82e0
sb3827 <mmk275@naver.com>
2023-11-30 19:37:18 +0900

    init

---
+++ src/main/java/com/yayum/tour_info_service_server/TourInfoServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.yayum.tour_info_service_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourInfoServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourInfoServiceServerApplication.class, args);
+	}
+
+}
