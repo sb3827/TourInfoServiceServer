2fd48152907fac85ed96a95883e340293e9f1e39
이해창 <gockd9968@naver.com>
2023-12-05 19:12:14 +0900

    Board Entity 추가
    Board Entity 추가

---
+++ src/main/java/com/yayum/tour_info_service_server/entity/Board.java
@@ -0,0 +1,31 @@
+package com.yayum.tour_info_service_server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+    @Lob
+    private String content;
+    private Boolean isAd;
+    private LocalDate regDate;
+    private LocalDate modDate;
+    private Boolean isCourse;
+    private Double score;
+    private int likes;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    private Member mno;
+}
