23933515d6d0a3a8994de3fab3eac906d35c47f0
honghyibeom <ghdgmlqja1@naver.com>
2023-12-05 19:28:38 +0900

    Follow Table 과 PK 생성

---
+++ src/main/java/com/yayum/tour_info_service_server/entity/Follow.java
@@ -0,0 +1,19 @@
+package com.yayum.tour_info_service_server.entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import lombok.*;
+
+@Getter
+@Builder
+@Embeddable
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Follow{
+
+  @EmbeddedId
+  private FollowPK followPk;
+
+}
