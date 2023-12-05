9a1d7c7f54c26121581b89fd7d68edd4a27c37a7
이해창 <gockd9968@naver.com>
2023-12-05 19:12:23 +0900

    Report Entity 추가
    Report Entity 추가

---
+++ src/main/java/com/yayum/tour_info_service_server/entity/Report.java
@@ -0,0 +1,29 @@
+package com.yayum.tour_info_service_server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.ibatis.annotations.Many;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sno;
+    private Boolean isDone;
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member mno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board bno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Reply rno;
+
+}
