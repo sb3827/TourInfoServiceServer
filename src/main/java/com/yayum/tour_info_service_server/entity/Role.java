7f352929aac0afa3101887b10bc559b65d3ab6bd
honghyibeom <ghdgmlqja1@naver.com>
2023-12-05 19:27:53 +0900

    Role Table 과 PK 생성

---
+++ src/main/java/com/yayum/tour_info_service_server/entity/Role.java
@@ -0,0 +1,19 @@
+package com.yayum.tour_info_service_server.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class Role {
+
+  @EmbeddedId
+  private RolePK rolePk;
+
+
+}
