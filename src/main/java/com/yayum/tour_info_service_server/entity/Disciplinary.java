3c7995edcbb68177baa505c972d0877664e04171
yununyu <myamoly10@gmail.com>
2023-12-06 15:40:58 +0900

    Disciplinary entity

---
+++ src/main/java/com/yayum/tour_info_service_server/entity/Disciplinary.java
@@ -0,0 +1,26 @@
+package com.yayum.tour_info_service_server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.lang.reflect.Member;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Builder
+@ToString
+public class Disciplinary {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long dno;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Member mno;
+
+  private String reason;
+  private LocalDate strDate;  // 제재 시작 일자
+  private LocalDate expDate;  // 제재 종료 일자
+}
