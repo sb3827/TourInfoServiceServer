de84eb6417fa54c33886cdf57c3f8328c1d65e95
honghyibeom <ghdgmlqja1@naver.com>
2023-12-06 15:22:10 +0900

    ROLE ENTITY 수정

--- src/main/java/com/yayum/tour_info_service_server/entity/Member.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Member.java
@@ -1,12 +1,11 @@
 package com.yayum.tour_info_service_server.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -29,5 +28,13 @@ public class Member {
   private int warn;
   private int disciplinary;
   private int bussinessId;
+
+  @ElementCollection(fetch = FetchType.LAZY)
+  @Builder.Default
+  private Set<Role> roleSet = new HashSet<>();
+
+  public void addMemberRole(Role role){
+    roleSet.add(role);
+  }
 
 }
--- src/main/java/com/yayum/tour_info_service_server/entity/Role.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Role.java
@@ -1,19 +1,6 @@
 package com.yayum.tour_info_service_server.entity;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import lombok.*;
-
-@Entity
-@Builder
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@ToString
-public class Role {
-
-  @EmbeddedId
-  private RolePK rolePk;
-
+public enum Role {
+  USER, MANAGER, ADMIN, BUSINESSPERSON
 
 }
