8474c208e43a4a689c48092abbd3f9c4669803a3
honghyibeom <ghdgmlqja1@naver.com>
2023-12-08 19:47:38 +0900

    BoardController 생성

---
+++ src/main/java/com/yayum/tour_info_service_server/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.yayum.tour_info_service_server.controller;
+
+import com.yayum.tour_info_service_server.dto.BoardDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+  //장소 포스팅 등록
+//  @PostMapping("/board/place/posting/register")
+//  public String placeRegisterPost (BoardDTO boardDTO) {
+//
+//  }
+}
