af17fda325e31ac7a90d53ae03b1831236cda20f
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-11 17:39:32 +0900

    폴더 전체 내용 들고오는 dto 작성

---
+++ src/main/java/com/yayum/tour_info_service_server/dto/FolderAllDTO.java
@@ -0,0 +1,18 @@
+package com.yayum.tour_info_service_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FolderAllDTO {
+    private Long fno;
+    private String title;
+    private Long pno;
+    private String name;
+
+}
