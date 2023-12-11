386d1dac174013d6e74f71d27ad7d6a33ecd6997
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-11 17:37:57 +0900

    private 추가

--- src/main/java/com/yayum/tour_info_service_server/dto/CartDTO.java
+++ src/main/java/com/yayum/tour_info_service_server/dto/CartDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartDTO {
-    Long mno;
-    Long pno;
-    Long fno;
+    private Long mno;
+    private Long pno;
+    private Long fno;
 }
