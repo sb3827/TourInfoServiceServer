aeb2473ad9c6e471adfbed84879d6aa92230cc27
honghyibeom <ghdgmlqja1@naver.com>
2023-12-14 16:55:11 +0900

    게시글 추가,삭제 기능

--- src/main/java/com/yayum/tour_info_service_server/service/BoardService.java
+++ src/main/java/com/yayum/tour_info_service_server/service/BoardService.java
@@ -1,11 +1,47 @@
 package com.yayum.tour_info_service_server.service;
 
 import com.yayum.tour_info_service_server.dto.BoardDTO;
+import com.yayum.tour_info_service_server.entity.Board;
+import com.yayum.tour_info_service_server.entity.Member;
 
 import java.util.Map;
 
 public interface BoardService {
 
   Long register(BoardDTO boardDTO);
+
+  Long removeWithReplies(Long bno);
+
+  void modify(BoardDTO boardDTO);
+
+  default Board dtoToEntity (BoardDTO boardDTO) {
+    Board board = Board.builder()
+        .bno(boardDTO.getBno())
+        .title(boardDTO.getTitle())
+        .content(boardDTO.getContent())
+        .isAd(boardDTO.getIsAd())
+        .isCourse(boardDTO.getIsCourse())
+        .score(boardDTO.getScore())
+        .likes(boardDTO.getLikes())
+        .writer(Member.builder().mno(boardDTO.getWriter()).build())
+        .build();
+    return board;
+  }
+
+  default BoardDTO entityToDto (Board board) {
+    BoardDTO boardDTO = BoardDTO.builder()
+        .bno(board.getBno())
+        .title(board.getTitle())
+        .content(board.getContent())
+        .isAd(board.getIsAd())
+        .isCourse(board.getIsCourse())
+        .content(board.getContent())
+        .score(board.getScore())
+        .likes(board.getLikes())
+        .modDate(board.getModDate())
+        .regDate(board.getRegDate())
+        .build();
+    return boardDTO;
+  }
 
 }
