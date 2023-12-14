fa19ee66328b281ef6012d96d16865bd30e94f4c
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-14 22:14:46 +0900

    report controller 틀 완성

--- src/main/java/com/yayum/tour_info_service_server/controller/ReportController.java
+++ src/main/java/com/yayum/tour_info_service_server/controller/ReportController.java
@@ -1,18 +1,15 @@
 package com.yayum.tour_info_service_server.controller;
 
-import com.yayum.tour_info_service_server.dto.DisciplinaryDTO;
-import com.yayum.tour_info_service_server.dto.ReportDTO;
-import com.yayum.tour_info_service_server.dto.ReportFilterDTO;
-import com.yayum.tour_info_service_server.repository.ReportRepository;
+import com.yayum.tour_info_service_server.dto.*;
 import com.yayum.tour_info_service_server.service.ReportService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/report")
@@ -21,11 +18,12 @@ public class ReportController {
 
     private final ReportService reportService;
 
-
     //필터 조회
-    @GetMapping(value = "/",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ReportDTO>>reportFilter(@RequestParam ReportFilterDTO reportFilterDTO){
-        return new ResponseEntity<>(reportService.reportFilter(reportFilterDTO),HttpStatus.OK);
+    @GetMapping(value = "",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ReportResponseDTO>>reportFilter(@RequestParam String filter,@RequestParam String search){
+        List<ReportResponseDTO> result=reportService.reportFilter(filter,search);
+        System.out.println(result);
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
 
     //신고 정보 조회
@@ -34,7 +32,7 @@ public class ReportController {
         return new ResponseEntity<>(reportService.reportDetail(sno),HttpStatus.OK);
     }
 
-    //회원신고 내역 전체 조회
+    //회원제재 내역 전체 조회
     @GetMapping(value = "/count",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Object>> disciplinaryAll(@RequestParam Long mno){
         return new ResponseEntity<>(reportService.disciplinaryUserData(mno),HttpStatus.OK);
@@ -42,19 +40,21 @@ public class ReportController {
 
     //신고
     @PostMapping(value = "/register",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Long> report(@RequestBody DisciplinaryDTO disciplinaryDTO){
-        return new ResponseEntity<>(reportService.disciplinary(disciplinaryDTO),HttpStatus.OK);
+    public ResponseEntity<Long> report(@RequestBody ReportRequestDTO reportRequestDTO){
+        return new ResponseEntity<>(reportService.report(reportRequestDTO),HttpStatus.OK);
     }
 
-    //신고 상태 변경
-    @PutMapping(value = "/update",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Long> reportIsDone(@RequestBody Long sno){
+    //신고 확인
+    @PutMapping(value = "/update/{sno}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> reportIsDone(@PathVariable Long sno){
+        System.out.println(sno);
         return new ResponseEntity<>(reportService.reportUpdate(sno),HttpStatus.OK);
     }
 
     //제재
     @PostMapping(value="/disciplinary",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Long> discoplinary(@RequestBody DisciplinaryDTO disciplinaryDTO){
-        return new ResponseEntity<>(reportService.disciplinary(disciplinaryDTO),HttpStatus.OK);
+    public ResponseEntity<Long> disciplinary(@RequestBody DisciplinaryRequestDTO disciplinaryRequestDTO){
+        reportService.reportUpdate(disciplinaryRequestDTO.getSno());
+        return new ResponseEntity<>(reportService.disciplinary(disciplinaryRequestDTO),HttpStatus.OK);
     }
 }
