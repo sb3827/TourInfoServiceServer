764bce5e9f87788462f58482fb37395fef03eed9
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-14 22:16:51 +0900

    Request에 필요한 DTO

---
+++ src/main/java/com/yayum/tour_info_service_server/dto/DisciplinaryRequestDTO.java
@@ -0,0 +1,16 @@
+package com.yayum.tour_info_service_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DisciplinaryRequestDTO {
+    private Long sno;
+    private Long mno;
+    private String reason;
+}
