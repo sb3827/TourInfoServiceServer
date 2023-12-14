a36204ad452d304c7a316ef434976292db996bae
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-14 22:14:15 +0900

    unique 만들어줌 같은 회원이 같은 게시글이나 댓글 신고할 경우 계속 데이터가 생성되는 오류 발생으로 인해 신고자와 게시글, 신고자와 댓글을 unique 키를 생성하여 계속 신고할 수 있는 오류 해결

--- src/main/java/com/yayum/tour_info_service_server/entity/Report.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Report.java
@@ -6,6 +6,11 @@ import org.apache.ibatis.annotations.Many;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
+//같은 사용자가 같은 게시글 또는 댓글을 여러번 신고불가능하도록 unique 설정
+@Table(name = "report", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"complainant_mno", "board_bno"}),
+        @UniqueConstraint(columnNames = {"complainant_mno","reply_rno"})
+})
 @Builder
 @Getter
 @NoArgsConstructor
@@ -16,8 +21,8 @@ public class Report extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long sno;
 
-    @ColumnDefault("false")
     @Column(nullable = false)
+    @ColumnDefault("false")
     private Boolean isDone;
 
     @Column(columnDefinition = "TEXT",nullable = false)
@@ -31,9 +36,11 @@ public class Report extends BaseEntity{
     private Long defendant_mno;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_bno")
     private Board board;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_rno")
     private Reply reply;
 
     public void changeIsDone(Boolean isDone){this.isDone=isDone;};
