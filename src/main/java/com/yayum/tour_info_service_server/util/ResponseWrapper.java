1c0d07701a968be48d6201c706af249d35df052e
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-15 17:08:06 +0900

    Response Wrpper 클래스 추가

---
+++ src/main/java/com/yayum/tour_info_service_server/util/ResponseWrapper.java
@@ -0,0 +1,13 @@
+package com.yayum.tour_info_service_server.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseWrapper<T> {
+    private boolean result;
+    private T data;
+}
