dde36127a1e9b3e26c467f10b0d7c5054b7906b7
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-15 17:34:11 +0900

    필요없는 코드 삭제

--- src/main/java/com/yayum/tour_info_service_server/repository/CartRepository.java
+++ src/main/java/com/yayum/tour_info_service_server/repository/CartRepository.java
@@ -2,6 +2,7 @@ package com.yayum.tour_info_service_server.repository;
 
 import com.yayum.tour_info_service_server.dto.CartDTO;
 import com.yayum.tour_info_service_server.entity.Cart;
+import com.yayum.tour_info_service_server.entity.CartPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, CartDTO> {
