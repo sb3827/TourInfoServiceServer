4a5277edcbcf47b074be1c5c4d90f7e40ca70e51
sb3827 <mmk275@naver.com>
2023-12-18 17:08:08 +0900

    security jwt filter 추가 및 설정

--- src/main/java/com/yayum/tour_info_service_server/config/SecurityConfig.java
+++ src/main/java/com/yayum/tour_info_service_server/config/SecurityConfig.java
@@ -1,28 +1,76 @@
 package com.yayum.tour_info_service_server.config;
 
+import com.yayum.tour_info_service_server.config.jwt.JwtAuthenticationEntryPoint;
+import com.yayum.tour_info_service_server.config.jwt.TokenProvider;
+import com.yayum.tour_info_service_server.security.filter.JwtFilter;
+import com.yayum.tour_info_service_server.security.handler.JwtAccessDeniedHandler;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true) // todo 완료 후 debug 제거
 @EnableMethodSecurity
 @Log4j2
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     protected SecurityFilterChain config(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+
                 .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.requestMatchers("/auth/getTest").authenticated();
                     authorizeRequests.anyRequest().permitAll();
                 })
+
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+
                 .build();
     }
+
+    @Bean
+    public JwtFilter jwtFilter(){
+        return new JwtFilter(tokenProvider);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
