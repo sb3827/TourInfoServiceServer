5d6e03234fddff444e2ce22a4ffbe9376a8f50c4
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-12-18 14:50:11 +0900

    ResponseWrapper -> ResponseDTO로 수정

--- src/main/java/com/yayum/tour_info_service_server/controller/FolderController.java
+++ src/main/java/com/yayum/tour_info_service_server/controller/FolderController.java
@@ -5,12 +5,11 @@ import com.yayum.tour_info_service_server.dto.FolderAllDTO;
 import com.yayum.tour_info_service_server.dto.FolderDTO;
 import com.yayum.tour_info_service_server.dto.FolderRegistDTO;
 import com.yayum.tour_info_service_server.service.FolderService;
-import com.yayum.tour_info_service_server.util.ResponseWrapper;
+import com.yayum.tour_info_service_server.dto.ResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,24 +23,24 @@ public class FolderController {
 
     //폴더 내용 모두 들고오기
     @GetMapping(value = "/all/{mno}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<List<FolderAllDTO>>> allFolder(@PathVariable Long mno){
+    public ResponseEntity<ResponseDTO<List<FolderAllDTO>>> allFolder(@PathVariable Long mno){
         List<FolderAllDTO> data=folderService.getAllFolder(mno);
-        ResponseWrapper<List<FolderAllDTO>> response=new ResponseWrapper<>(true,data);
+        ResponseDTO<List<FolderAllDTO>> response=new ResponseDTO<>(true,data);
       return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     //폴더명 모두 조회
     @GetMapping(value = "/title/{mno}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<List<String>>>folderNames(@PathVariable Long mno){
+    public ResponseEntity<ResponseDTO<List<String>>>folderNames(@PathVariable Long mno){
         List<String> data=folderService.getTitle(mno);
-        ResponseWrapper<List<String>> response=new ResponseWrapper<>(true,data);
+        ResponseDTO<List<String>> response=new ResponseDTO<>(true,data);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     //폴더 등록
     @PostMapping(value = "/register",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<Long>>register(@RequestBody FolderRegistDTO folderRegistDTO){
-        ResponseWrapper response=new ResponseWrapper<>(false,null);
+    public ResponseEntity<ResponseDTO<Long>>register(@RequestBody FolderRegistDTO folderRegistDTO){
+        ResponseDTO response=new ResponseDTO<>(false,null);
         Long data=folderService.register(folderRegistDTO);
         if(data>0){
             response.setResult(true);
@@ -53,8 +52,8 @@ public class FolderController {
 
     //폴더명 수정
     @PutMapping(value = "/update",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<Long>>modify(@RequestBody FolderDTO folderDTO){
-        ResponseWrapper response=new ResponseWrapper<>(false,null);
+    public ResponseEntity<ResponseDTO<Long>>modify(@RequestBody FolderDTO folderDTO){
+        ResponseDTO response=new ResponseDTO<>(false,null);
         Long data=folderService.modify(folderDTO);
         if(data!=-1l){
             response.setResult(true);
@@ -66,8 +65,8 @@ public class FolderController {
 
     //폴더 삭제
     @DeleteMapping(value = "/delete/{fno}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<Long>> remove(@PathVariable Long fno){
-        ResponseWrapper response = new ResponseWrapper(false,null);
+    public ResponseEntity<ResponseDTO<Long>> remove(@PathVariable Long fno){
+        ResponseDTO response = new ResponseDTO(false,null);
         Long data=folderService.remove(fno);
         if (data!=-1l){
             response.setResult(true);
@@ -79,8 +78,8 @@ public class FolderController {
 
     //스팟 등록
     @PostMapping(value = "/spot-append",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<Long>>spotAdd(@RequestBody CartDTO cartDTO){
-        ResponseWrapper response=new ResponseWrapper(false,null);
+    public ResponseEntity<ResponseDTO<Long>>spotAdd(@RequestBody CartDTO cartDTO){
+        ResponseDTO response=new ResponseDTO(false,null);
         Long data=folderService.addSpot(cartDTO);
         if (data!=-1l){
             response.setResult(true);
@@ -92,8 +91,8 @@ public class FolderController {
 
     //스팟 삭제
     @DeleteMapping(value="/spot-delete",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseWrapper<String>>spotDelete(@RequestBody CartDTO cartDTO){
-        ResponseWrapper response=new ResponseWrapper(false,null);
+    public ResponseEntity<ResponseDTO<String>>spotDelete(@RequestBody CartDTO cartDTO){
+        ResponseDTO response=new ResponseDTO(false,null);
         String data=folderService.deleteSpot(cartDTO);
         if (data!=null){
             response.setResult(true);
