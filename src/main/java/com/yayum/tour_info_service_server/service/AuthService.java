14e548b7f32b11a1928187e7b1ab2ace87eb7265
sb3827 <mmk275@naver.com>
2023-12-19 18:34:55 +0900

    rename

--- src/main/java/com/yayum/tour_info_service_server/service/AuthService.java
+++ src/main/java/com/yayum/tour_info_service_server/service/AuthService.java
@@ -3,20 +3,18 @@ package com.yayum.tour_info_service_server.service;
 import com.yayum.tour_info_service_server.dto.*;
 import com.yayum.tour_info_service_server.entity.Member;
 import com.yayum.tour_info_service_server.entity.Role;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Map;
 import java.util.Objects;
 
 public interface AuthService {
     String login(JwtRequestDTO requestDTO);
-    Long signup(SignupDTO signupDTO);
+    Long signup(SignupRequestDTO signupDTO);
     Boolean emailCheck(String email);
     String findEmail(String name, String phone);
     ResponseDTO changePassword(ChangeMemberDTO changeMemberDTO);
     ResponseDTO resetPassword(MemberDTO memberDTO);
-    default Member dtoToEntity(SignupDTO signupDTO) {
+    Boolean checkValidate();
+    default Member signupDtoToEntity(SignupRequestDTO signupDTO) {
         Member member;
 
         if (Objects.equals(signupDTO.getRole(), "MEMBER")) {
