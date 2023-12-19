77fb304662881f4dd4fb837cb01b4fa6f3e8b5f9
sb3827 <mmk275@naver.com>
2023-12-19 18:31:37 +0900

    add method sending validate url

--- src/main/java/com/yayum/tour_info_service_server/service/MailServiceImpl.java
+++ src/main/java/com/yayum/tour_info_service_server/service/MailServiceImpl.java
@@ -1,13 +1,20 @@
 package com.yayum.tour_info_service_server.service;
 
 import com.yayum.tour_info_service_server.entity.Member;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 @Log4j2
@@ -55,5 +62,28 @@ public class MailServiceImpl implements MailService {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    @Override
+    public void sendValidateUrl(String email, String name, String token) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        String title = name + "님의 yayum 이메일 인증";
+        String text = "<h1>"+ name + "님의 yayum 가입을 축하드립니다.</h1>" +
+                "<p>보내드린 <a href='http://localhost:8080/auth/email/validation?email=" + email + "&token=" + token +
+                "'>주소</a> 로 10분 이내로 접속하여 인증해 주세요.</p>" +
+                "<p>가입하신 내역이 없다면 이 메일을 무시하셔도 됩니다.</p>" +
+                "<meta charset='UTF-8'>";
+
+
+        helper.setFrom(new InternetAddress("noreply@yayum.com", "yayum", "UTF-8"));
+        //받는사람
+        helper.setTo(email);
+        //제목
+        helper.setSubject(title);
+        //내용
+        helper.setText(text, true);
+
+        javaMailSender.send(message);
     }
 }
