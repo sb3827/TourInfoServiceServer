4efa6f3d0d0eb7876561f203be5c23721bafc74f
sb3827 <mmk275@naver.com>
2023-12-19 19:13:41 +0900

    이메일 검증 getmapping 추가

--- src/main/java/com/yayum/tour_info_service_server/controller/AuthController.java
+++ src/main/java/com/yayum/tour_info_service_server/controller/AuthController.java
@@ -1,24 +1,21 @@
 package com.yayum.tour_info_service_server.controller;
 
 import com.yayum.tour_info_service_server.dto.*;
-import com.yayum.tour_info_service_server.security.dto.AuthMemberDTO;
+import com.yayum.tour_info_service_server.security.util.SecurityUtil;
 import com.yayum.tour_info_service_server.service.AuthService;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.mail.MailException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
-import javax.xml.transform.Result;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,13 +26,12 @@ import java.util.Map;
 public class AuthController {
     private final AuthService authService;
 
-    // security context test get mapping
+    // security context 의 principal 정보 가져오기
     // todo delete
     @GetMapping("/getTest")
-    public ResponseEntity<String> getTest(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        log.info(authentication.getName());
-        log.info(((AuthMemberDTO) authentication.getPrincipal()).getMno());
+    public ResponseEntity<String> getTest() {
+        log.info(SecurityUtil.getCurrentMemberEmail());
+        log.info(SecurityUtil.getCurrentMemberMno());
         return new ResponseEntity<>("test", HttpStatus.OK);
     }
 
@@ -57,6 +53,7 @@ public class AuthController {
 
     @PostMapping("/logout")
     public ResponseDTO logout(HttpServletRequest request, HttpServletResponse response) {
+        // todo refresh token 생성시 refresh token 삭제 logic add
         try {
             new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
             return new ResponseDTO("success", true);
@@ -66,36 +63,44 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Map<String, Long>> register(@RequestBody SignupDTO signupDTO) {
+    public ResponseEntity<Map<String, Long>> register(@RequestBody SignupRequestDTO signupDTO) {
         Map<String, Long> responseMap = new HashMap<>();
-        Long num = authService.signup(signupDTO);
 
-        responseMap.put("mno", num);
-
-        if (num >0) {
-            return new ResponseEntity<>(responseMap, HttpStatus.OK);
+        try {
+            Long num = authService.signup(signupDTO);
+            responseMap.put("mno", num);
+        } catch (MailException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
     }
 
     @PostMapping("/email/check")
-    public ResponseEntity<Map<String, Boolean>> checkDuplicate(@RequestBody MemberDTO memberDTO){
-//        log.info(memberDTO.getEmail());
+    public ResponseEntity<Map<String, Boolean>> checkDuplicate(@RequestBody MemberDTO memberDTO) {
         Map<String, Boolean> responseMap = new HashMap<>();
         Boolean isDuplicate = authService.emailCheck(memberDTO.getEmail());
 
         responseMap.put("isDuplicate", isDuplicate);
 
-        return new ResponseEntity(responseMap, HttpStatus.OK);
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
     }
 
     @PostMapping("/email/validation")
-    public void checkValidate(){
+    public void checkValidate() {
 
     }
 
+    @GetMapping("/email/validation")
+    public ModelAndView checkValtidate(@RequestParam("email") String email, @RequestParam("token") String token) {
+        log.info("email: "+email);
+        log.info("token: "+token);
+        return new ModelAndView(new RedirectView("http://localhost:3000/login"));
+    }
+
     @PostMapping("/email/find")
-    public ResponseEntity<Map<String, String>> findMail(@RequestBody MemberDTO memberDTO){
+    public ResponseEntity<Map<String, String>> findMail(@RequestBody MemberDTO memberDTO) {
         Map<String, String> responseMap = new HashMap<>();
         String email = authService.findEmail(memberDTO.getName(), memberDTO.getPhone());
 
@@ -106,9 +111,12 @@ public class AuthController {
 
     @PostMapping("password/change")
     public ResponseEntity<ResponseDTO> changePassword(@RequestBody ChangeMemberDTO changeMemberDTO) {
-        ResponseDTO result = authService.changePassword(changeMemberDTO);
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        if (SecurityUtil.validateEmail(changeMemberDTO.getEmail())) {
+            ResponseDTO result = authService.changePassword(changeMemberDTO);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("password/lost")
