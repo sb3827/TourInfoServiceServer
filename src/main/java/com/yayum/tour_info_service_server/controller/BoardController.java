8d9ee492a99eea4616b8b5728ddbd0ba856b2427
honghyibeom <ghdgmlqja1@naver.com>
2023-12-19 15:14:49 +0900

    등록, 삭제, 수정 controller

--- src/main/java/com/yayum/tour_info_service_server/controller/BoardController.java
+++ src/main/java/com/yayum/tour_info_service_server/controller/BoardController.java
@@ -16,19 +16,25 @@ public class BoardController {
   private final BoardService boardService;
 
   // 장소,코스 포스팅 등록
-  @PostMapping({"/place/posting/register","/course/posting/register"})
+  @PostMapping(value = {"/place/posting/register","/course/posting/register"})
   public ResponseEntity<Long> registerPost (@RequestBody BoardDTO boardDTO) {
     Long bno = boardService.register(boardDTO);
     return  new ResponseEntity<>(bno, HttpStatus.OK);
   }
 
   // 장소,코스 포스팅, 댓글 삭제
-  @DeleteMapping({"/place/posting/delete/{bno}","/course/posting/delete/{bno}"})
+  @DeleteMapping(value = {"/place/posting/delete/{bno}","/course/posting/delete/{bno}"})
   public ResponseEntity<Long> remove(@PathVariable("bno") Long bno) {
     log.info("delete...bno: "+bno);
-    boardService.removeWithReplies(bno);
+    boardService.remove(bno);
     return new ResponseEntity<>(bno,HttpStatus.OK);
   }
 
-
+  // 장소,코스 포스팅 수정
+  @PutMapping(value = {"/place/posting/update","/course/posting/update"})
+  public ResponseEntity<Long> modify(@RequestBody BoardDTO boardDTO){
+   log.info("modify...dto: "+boardDTO);
+   Long bno = boardService.modify(boardDTO);
+   return new ResponseEntity<>(bno,HttpStatus.OK);
+  }
 }
