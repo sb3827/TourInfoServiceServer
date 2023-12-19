eb0593befb3f58cfe8466f64279cc020803c3e09
honghyibeom <ghdgmlqja1@naver.com>
2023-12-19 15:22:38 +0900

    pull develop

--- src/main/java/com/yayum/tour_info_service_server/entity/Report.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Report.java
@@ -6,29 +6,43 @@ import org.apache.ibatis.annotations.Many;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
+//같은 사용자가 같은 게시글 또는 댓글을 여러번 신고불가능하도록 unique 설정
+@Table(name = "report", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"complainant_mno", "board_bno"}),
+    @UniqueConstraint(columnNames = {"complainant_mno","reply_rno"})
+})
 @Builder
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Report {
+public class Report extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long sno;
 
+    @Column(nullable = false)
     @ColumnDefault("false")
     private Boolean isDone;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
 
     @Column(nullable = false)
     private String message;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Member complainant;
+    private Long complainant_mno;
+
+    private Long defendant_mno;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_bno")
     private Board board;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_rno")
     private Reply reply;
+
+    public void changeIsDone(Boolean isDone){this.isDone=isDone;};
 
 }
