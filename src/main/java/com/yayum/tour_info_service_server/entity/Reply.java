91494d13d04af91a2dd7591da404fd9aec26b49a
yununyu <myamoly10@gmail.com>
2023-12-20 17:20:47 +0900

    대댓글 리스트 삭제. 필요없는 로직 삭제.

--- src/main/java/com/yayum/tour_info_service_server/entity/Reply.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Reply.java
@@ -27,27 +27,18 @@ public class Reply extends BaseEntity{
 
   @Column(nullable = false, length =1000)
   private String text;    // 댓글 내용
-  @ManyToOne(fetch = FetchType.LAZY)
 
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "parent_rno")
   private Reply parent;   // 부모 댓글
 
-  @OneToMany(mappedBy = "parent")
-  private List<Reply> childrenList = new ArrayList<>();   // 자식댓글
+
 
   public void changeText(String text) {
     this.text = text;
   }
 
-
-  // 자식댓글을 특정 부모댓글의 자식댓글로 설정하는 로직 :: service 의 대댓글 작성 기능에 사용
-  public void setAsChildren(Reply parent) {
-    this.parent = parent;
-    parent.addChild(this);
+  public void changeMember(Member member) {
+    this.member = member;
   }
-
-  public void addChild(Reply child) {
-    childrenList.add(child);
-  }
-
 }
