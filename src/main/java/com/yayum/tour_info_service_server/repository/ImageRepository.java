4d3c601810d18205ef806fa1f78366f64084fb70
honghyibeom <ghdgmlqja1@naver.com>
2023-12-26 17:17:51 +0900

    Board 수정

--- src/main/java/com/yayum/tour_info_service_server/repository/ImageRepository.java
+++ src/main/java/com/yayum/tour_info_service_server/repository/ImageRepository.java
@@ -1,13 +1,26 @@
 package com.yayum.tour_info_service_server.repository;
 
 import com.yayum.tour_info_service_server.entity.Image;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
+  //board 삭제시 해당하는 이미지삭제
   @Modifying // update, delete를 할 때는 무조건 붙인다.
   @Query("delete from Image i where i.board.bno=:bno")
   void deleteByBno(Long bno);
+
+
+  // bno 넣을때 해당하는 ino 반환
+  @Transactional
+  @Query("SELECT i FROM Image i WHERE i.board.bno=:bno")
+  List<Image> selectImageByBno(@Param("bno") Long bno);
+
+
 }
--- src/test/java/com/yayum/tour_info_service_server/repository/ImageRepositoryTests.java
+++ src/test/java/com/yayum/tour_info_service_server/repository/ImageRepositoryTests.java
@@ -4,10 +4,10 @@ import com.yayum.tour_info_service_server.entity.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ImageRepositoryTests {
@@ -16,20 +16,26 @@ class ImageRepositoryTests {
 
   @Test
   public void insert() {
-    IntStream.rangeClosed(1, 1).forEach(i -> {
+    IntStream.rangeClosed(1, 3).forEach(i -> {
 
-      Board board = Board.builder().bno(12L).build();
+      Board board = Board.builder().bno(3L).build();
 
       Place place = Place.builder().pno(1L).build();
 
       Image image = Image.builder()
           .board(board)
           .ino(place.getPno())
-          .src("test")
+          .src("test2")
           .build();
       imageRepository.save(image);
     });
   }
 
+  @Transactional
+  @Test
+  public void inoByBno() {
+    List<Image> result = imageRepository.selectImageByBno(3L);
+      System.out.println(result);
+  }
 
 }
