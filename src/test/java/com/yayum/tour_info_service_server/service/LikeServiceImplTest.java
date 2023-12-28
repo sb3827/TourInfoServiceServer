184313c2a8add5aaff13b50d20088b72963c25c1
sb3827 <mmk275@naver.com>
2023-12-28 23:01:42 +0900

    like, unlike service logic test

---
+++ src/test/java/com/yayum/tour_info_service_server/service/LikeServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.yayum.tour_info_service_server.service;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Log4j2
+class LikeServiceImplTest {
+    @Autowired
+    LikeService likeService;
+
+    @Test
+    public void likeBoardTest() {
+        try {
+            log.info(likeService.likeBoard(5L, 3L));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void unLikeBoardTest() {
+        try {
+            log.info(likeService.unLikeBoard(5L, 3L));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
