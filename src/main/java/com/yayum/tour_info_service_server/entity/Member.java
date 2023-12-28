1c0fc3d4bdc50fc508470b1ba15b9c3e2993294a
이해창 <gockd9968@naver.com>
2023-12-28 16:00:13 +0900

    회원정보수정을 위해 setter 추가
    회원정보수정을 위해 setter 추가

--- src/main/java/com/yayum/tour_info_service_server/entity/Member.java
+++ src/main/java/com/yayum/tour_info_service_server/entity/Member.java
@@ -73,4 +73,16 @@ public class Member extends BaseEntity {
   public void changeIsReset() {
     this.isReset = !this.isReset;
   }
+
+  public void changeName(String name) {
+    this.name = name;
+  }
+
+  public void changePhone(String phone) {
+    this.phone = phone;
+  }
+
+  public void changeImage(String image) {
+    this.image = image;
+  }
 }
