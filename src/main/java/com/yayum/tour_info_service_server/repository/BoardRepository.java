0333222bbee071b44da1827920a1b5f33b58db01
이해창 <gockd9968@naver.com>
2023-12-28 16:07:12 +0900

    place 삭제를 위해 board 삭제
    place 삭제를 위해 board 삭제
    게시글 정보(장소/코스)
    pno로 작성한 bno들 반환

--- src/main/java/com/yayum/tour_info_service_server/repository/BoardRepository.java
+++ src/main/java/com/yayum/tour_info_service_server/repository/BoardRepository.java
@@ -11,15 +11,18 @@ import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    // 게시글이 코스 게시글이면 true, 장소 게시글이면 false
+
     @Query("select b.isCourse from Board b where b.bno in (select bp.boardPlacePK.board.bno from BoardPlace bp where bp.place.pno = :pno)")
     Boolean boardIsCourse(Long pno);
 
+    // pno로 작성한 게시글 bno 반환
     @Query("select b.bno from Board b where b.bno in (select bp.boardPlacePK.board.bno from BoardPlace bp where bp.place.pno = :pno)")
     List<Long> returnBnos(Long pno);
 
+    // 게시글 삭제
     @Modifying
     @Transactional
     @Query("delete from Board b where b.bno in (select bp.boardPlacePK.board.bno from BoardPlace bp where bp.place.pno = :pno)")
     void removeBoard(Long pno);
-
 }
