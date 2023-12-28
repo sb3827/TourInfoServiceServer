9d0c713e689884ef37e2b35aa2b944b51d6d1396
이해창 <gockd9968@naver.com>
2023-12-28 16:06:00 +0900

    회원프로필조회 DTO
    회원프로필조회 DTO

---
+++ src/main/java/com/yayum/tour_info_service_server/dto/UserProfileDTO.java
@@ -0,0 +1,18 @@
+package com.yayum.tour_info_service_server.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserProfileDTO {
+    private Long mno;
+    private String name;
+    private Long followings;
+    private Long followers;
+    private Long cart;
+    private String image;
+
+}
