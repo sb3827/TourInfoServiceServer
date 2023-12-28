08c7824ad749618e886c0ef30d58078e9b9eca1d
이해창 <gockd9968@naver.com>
2023-12-28 16:09:24 +0900

    place 삭제를 위한 테스트
    게시글 삭제, 게시글 정보 (장소/코스), bno 반환

--- src/test/java/com/yayum/tour_info_service_server/repository/BoardRepositoryTests.java
+++ src/test/java/com/yayum/tour_info_service_server/repository/BoardRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.yayum.tour_info_service_server.repository;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,29 +17,22 @@ class BoardRepositoryTests {
     @Autowired
     BoardRepository boardRepository;
 
+    // 게시물 삭제 테스트
     @Test
     void remove(){
         boardRepository.removeBoard(1L);
     }
 
+    // 게시물 코스 게시글/ 장소 게시글 판별 테스트
     @Test
     void course(){
         System.out.println(boardRepository.boardIsCourse(5L));
     }
 
+    // bno 반환 테스트
     @Test
     void returnBno(){
         System.out.println(boardRepository.returnBnos(26L));
     }
-
-
-//    @Test
-//    void removeTest(){
-//        List<Long> bnosToRemove = boardRepository.returnBnos(4L);
-//
-//        for ( Long bnos : bnosToRemove){
-//            boardRepository.deleteById(bnos);
-//        }
-//    }
 
 }
