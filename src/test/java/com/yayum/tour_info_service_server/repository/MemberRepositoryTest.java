84535cb7be539390aac659e22cadaf1509ec6d33
이해창 <gockd9968@naver.com>
2023-12-28 16:10:04 +0900

    MemberRepository Test
    회원 검색, 회원가입대기 조회, 회원가입 승인 테스트

--- src/test/java/com/yayum/tour_info_service_server/repository/MemberRepositoryTest.java
+++ src/test/java/com/yayum/tour_info_service_server/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.yayum.tour_info_service_server.repository;
 
+import com.yayum.tour_info_service_server.dto.UserInfoDTO;
 import com.yayum.tour_info_service_server.entity.Member;
 import com.yayum.tour_info_service_server.entity.Role;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -57,7 +59,6 @@ class MemberRepositoryTest {
     // member+phone로 member 찾기 test
     public void findMemberbyNamePhoneTest() {
         Optional<Member> result = memberRepository.findMemberByNameAndPhone("희범", "010-1234-5678");
-
         if (result.isEmpty()){
             log.info("not found member");
         } else {
@@ -71,6 +72,29 @@ class MemberRepositoryTest {
     // email 중복 test
     public void existEmailTest(){
         log.info("is Duplicate?: "+memberRepository.existsByEmail("mmk2751@gmail.com"));
+    }
+
+    // 회원 검색 테스트
+    @Test
+    public void searchUserTest(){
+        List<Object[]> userlist= memberRepository.searchUser("백");
+        for(Object[] list : userlist){
+            log.info("user mno : " + list[0]);
+            log.info("user image : " + list[1]);
+            log.info("user name : " + list[2]);
+        }
+    }
+
+    // 회원가입 대기 조회 테스트
+    @Test
+    public void showTest(){
+        log.info(memberRepository.showJoinWaiting());
+    }
+
+    // 회원가입 승인 테스트
+    @Test
+    public void joinTest(){
+        memberRepository.joinMember(8L);
     }
 
 }
