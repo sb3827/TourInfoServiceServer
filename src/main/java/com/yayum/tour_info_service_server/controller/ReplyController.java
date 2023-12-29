ad7368ffcfca5eda204b4180b0660775742201ed
yununyu <myamoly10@gmail.com>
2023-12-29 14:41:39 +0900

    DB에서 저장이 실패 했을 때 ResponseEntity return을 처리하는 방식이 없는 것 같습니다. -> try catch 추가 +유저 댓글 삭제시 delete -> modify 사용

--- src/main/java/com/yayum/tour_info_service_server/controller/ReplyController.java
+++ src/main/java/com/yayum/tour_info_service_server/controller/ReplyController.java
@@ -1,7 +1,6 @@
 package com.yayum.tour_info_service_server.controller;
 
 import com.yayum.tour_info_service_server.dto.ReplyDTO;
-import com.yayum.tour_info_service_server.dto.ReportDTO;
 import com.yayum.tour_info_service_server.dto.ReportRequestDTO;
 import com.yayum.tour_info_service_server.dto.ResponseWrapDTO;
 import com.yayum.tour_info_service_server.security.util.SecurityUtil;
@@ -24,15 +23,15 @@ public class ReplyController {
   private final ReplyService replyService;
   private final ReportService reportService;
 
-  @GetMapping("/mno/{mno}")
-  public ResponseEntity<List<ReplyDTO>> getListByMno(@PathVariable("mno") Long mno) {
+  @GetMapping("/mno")
+  public ResponseEntity<List<ReplyDTO>> getListByMno(@RequestParam("mno") Long mno) {
     log.info("getReplyListByMno....");
     List<ReplyDTO> result = replyService.getListOfReplyByMember(mno);
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
-  @GetMapping("/bno/{bno}")
-  public ResponseEntity<List<ReplyDTO>> getListByBno(@PathVariable("bno") Long bno) {
+  @GetMapping("/bno")
+  public ResponseEntity<List<ReplyDTO>> getListByBno(@RequestParam("bno") Long bno) {
     log.info("getReplyListByBno....");
     List<ReplyDTO> result = replyService.getListOfReplyByBoard(bno);
     return new ResponseEntity<>(result, HttpStatus.OK);
@@ -41,48 +40,51 @@ public class ReplyController {
   @PostMapping("/register")
   public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
     log.info("saveReply... " + replyDTO);
-    replyService.saveReply(replyDTO);
+    try {
+      replyService.saveReply(replyDTO);
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      throw e;
+    }
     return new ResponseEntity<>(replyDTO.getRno(), HttpStatus.OK);
   }
 
-  @PutMapping("/update/{rno}")
-  public ResponseEntity<Long> update(@PathVariable Long rno, @RequestBody ReplyDTO replyDTO) {
-    if (SecurityUtil.validateMno(replyDTO.getMno())) {
-      log.info("updateReply...replyDTO : " + replyDTO);
-      replyDTO.setRno(rno);
-      replyService.modify(replyDTO);
-      return new ResponseEntity<>(rno, HttpStatus.OK);
-    } else {
+  @PutMapping("/update/rno")
+  public ResponseEntity<Long> update(@RequestParam("rno") Long rno, @RequestBody ReplyDTO replyDTO) {
+    if (!SecurityUtil.validateMno(replyDTO.getMno())) {
       log.info("NO MNO match@@");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    log.info("updateReply...replyDTO : " + replyDTO);
+    replyService.modify(replyDTO);
+    return new ResponseEntity<>(rno, HttpStatus.OK);
   }
 
-  @PutMapping("/delete/{rno}")
-  public ResponseEntity<Long> delete(@PathVariable Long rno, @RequestBody ReplyDTO replyDTO) {
-    if (SecurityUtil.validateMno(replyDTO.getMno())) {
-      log.info("deleteReply...replyDTO : " + replyDTO);
-      replyDTO.setRno(rno);
-      replyService.delete(replyDTO);
-      return new ResponseEntity<>(rno, HttpStatus.OK);
-    } else {
+  @PutMapping("/delete/rno")
+  public ResponseEntity<Long> delete(@RequestParam("rno") Long rno, @RequestBody ReplyDTO replyDTO) {
+    if (!SecurityUtil.validateMno(replyDTO.getMno())) {
       log.info("NO MNO match@@");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    log.info("deleteReply...replyDTO : " + replyDTO);
+    replyDTO.setText("삭제된 댓글입니다");
+    replyDTO.setMno(null);
+    replyService.modify(replyDTO);
+    return new ResponseEntity<>(rno, HttpStatus.OK);
   }
 
-  @PostMapping(value = "/report",produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseWrapDTO<Long>> report(@RequestBody ReportRequestDTO reportRequestDTO){
-    ResponseWrapDTO response=new ResponseWrapDTO(false,null);
-    Long data=reportService.report(reportRequestDTO);
-    if(data==1l){
+  @PostMapping(value = "/report", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ResponseWrapDTO<Long>> report(@RequestBody ReportRequestDTO reportRequestDTO) {
+    ResponseWrapDTO response = new ResponseWrapDTO(false, null);
+    Long data = reportService.report(reportRequestDTO);
+    if (data == 1l) {
       response.setResult(true);
       response.setData(data);
-      return new ResponseEntity<>(response,HttpStatus.OK);
+      return new ResponseEntity<>(response, HttpStatus.OK);
     }
     //만약 신고한 유저가 존재하지 않는다면 null을 전달받음
 
-    return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
   }
 
 }
