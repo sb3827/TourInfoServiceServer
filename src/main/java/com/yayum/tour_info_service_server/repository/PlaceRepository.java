55c166e5e273589a40d4769b4bf7edbcd9132696
이해창 <gockd9968@naver.com>
2024-01-02 11:07:04 +0900

    쿼리 수정
    쿼리 수정

--- src/main/java/com/yayum/tour_info_service_server/repository/PlaceRepository.java
+++ src/main/java/com/yayum/tour_info_service_server/repository/PlaceRepository.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     // 장소 검색
-    @Query("select p from Place p where (p.name like %:search% or p.localAddress like %:search% or p.roadAddress like %:search% or p.engAddress like %:search%) and " +
-            "(p.category = :filter or :filter = com.yayum.tour_info_service_server.entity.Category.ETC)")
+    @Query("select p from Place p where " +
+            "(:filter = 'ALL' or p.category = :filter) and " +
+            "(p.name like %:search% or p.localAddress like %:search% or " +
+            "p.roadAddress like %:search% or p.engAddress like %:search%)")
     List<Place> findPlace(Category filter, String search);
 }
