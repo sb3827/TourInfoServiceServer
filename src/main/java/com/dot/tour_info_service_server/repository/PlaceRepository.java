d35e11292e487049ccfe3368027ca536d879d173
이해창 <gockd9968@naver.com>
2024-01-09 10:16:31 +0900

    place 검색 쿼리 수정
    place 검색 쿼리 수정

--- src/main/java/com/dot/tour_info_service_server/repository/PlaceRepository.java
+++ src/main/java/com/dot/tour_info_service_server/repository/PlaceRepository.java
@@ -19,10 +19,14 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
             "limit 3")
     List<Object[]> mostLikePlace();
 
+
     @Query("select p.pno, p.name, p.lng, p.lat, p.roadAddress, p.localAddress, p.engAddress, p.category, p.cart, p.regDate, p.modDate, i.src " +
             "from Place p left outer join BoardPlace bp on p.pno = bp.place.pno " +
             "left outer join Image i on bp.boardPlacePK.board.bno = i.board.bno " +
-            "where :filter is null or p.category = :filter and " +
+            "where :filter is null and " +
+            "(p.name like %:search% or p.localAddress like %:search% or " +
+            "p.roadAddress like %:search% or p.engAddress like %:search%) " +
+            "or p.category = :filter and " +
             "(p.name like %:search% or p.localAddress like %:search% or " +
             "p.roadAddress like %:search% or p.engAddress like %:search%) " +
             "group by p.pno")
