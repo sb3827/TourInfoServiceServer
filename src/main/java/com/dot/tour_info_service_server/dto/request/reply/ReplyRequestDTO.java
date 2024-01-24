8202f5e0f1a4253fa519f07c56562a969d662e26
yununyu <myamoly10@gmail.com>
2024-01-24 13:23:01 +0900

    DTO 필요없는 변수 삭제 및 NotNull 추가

--- src/main/java/com/dot/tour_info_service_server/dto/request/reply/ReplyRequestDTO.java
+++ src/main/java/com/dot/tour_info_service_server/dto/request/reply/ReplyRequestDTO.java
@@ -1,23 +1,23 @@
 package com.dot.tour_info_service_server.dto.request.reply;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ReplyRequestDTO {
-  private Long rno;
   private Long parentRno;
   @NotBlank(message = "text cannot be blank")
   private String text;
+  @NotNull(message = "mno cannot be null")
   private Long mno;
+  @NotNull(message = "bno cannot be null")
   private Long bno;
-  private LocalDateTime regDate, modDate;
 }
