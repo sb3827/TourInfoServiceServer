0c4b4fb31264ac77f53b06eb8efd0eeaf04ba07e
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-02-01 12:39:58 +0900

    content 제거

--- src/main/java/com/dot/tour_info_service_server/repository/BoardRepository.java
+++ src/main/java/com/dot/tour_info_service_server/repository/BoardRepository.java
@@ -128,7 +128,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     //코스 검색 조회
     @Query("select b.bno, b.title, b.likes, b.score, b.writer.name, b.regDate, b.isAd from Board b " +
             "left outer join BoardPlace bp on b.bno = bp.boardPlacePK.board.bno " +
-            "where (b.title like %:search% or b.content like %:search% or " +
+            "where (b.title like %:search% or  " +
             "bp.place.name like %:search% or b.writer.name like %:search%) and b.isCourse = true " +
             "and b.isAd=:isAd " +
             "group by b.bno")
