398d819d3c2e2d834f0a27d4e2e81ade826ac5fa
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-02-05 22:41:38 +0900

    트리거와 프로시져 파일 분리

--- src/main/resources/procedure.sql
+++ src/main/resources/procedure.sql
@@ -1,26 +1,3 @@
-CREATE OR REPLACE TRIGGER `add_like` AFTER INSERT ON `board_like` FOR EACH ROW BEGIN
-UPDATE board SET board.likes = board.likes+1 WHERE board.bno = NEW.board_bno;
-END
-
-CREATE OR REPLACE TRIGGER `delete_like` AFTER DELETE ON `board_like` FOR EACH ROW BEGIN
-UPDATE board SET board.likes = board.likes-1 WHERE board.bno = OLD.board_bno;
-END
-
-CREATE OR REPLACE TRIGGER `add_cart` AFTER INSERT ON `cart` FOR EACH ROW BEGIN
-UPDATE place SET place.cart = place.cart+1 WHERE place.pno = NEW.place_pno;
-END
-
-CREATE OR REPLACE TRIGGER `delete_cart` AFTER DELETE ON `cart` FOR EACH ROW BEGIN
-UPDATE place SET place.cart = place.cart-1 WHERE place.pno = OLD.place_pno;
-END
-
-CREATE OR REPLACE TRIGGER `add_disciplinary` AFTER INSERT ON `disciplinary` FOR EACH ROW
-BEGIN
-UPDATE member set member.disciplinary = member.disciplinary+1 WHERE member.mno=NEW.member_mno;
-END
-
-DELIMITER $$
-
 CREATE or replace PROCEDURE Count_pno(in MNO bigint )
 BEGIN
 SELECT
@@ -74,7 +51,6 @@ WHERE
 END $$
 DELIMITER ;
 
-__
 
 BEGIN
 SELECT
